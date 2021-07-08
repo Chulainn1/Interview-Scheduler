56ca04f149ddee66d97ab015dc75e98b9ba54cff
Chulainn1 <leam.murphy1@gmail.com>
2021-07-08 13:31:57 -0600

    Form allows users to create or modify appointments. Clicking save triggers the onSave action. Cancel resets the state for thecomponent

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,14 +2,32 @@ import React, {useState} from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
+
 export default function Form(props) {
-  const setInterviewer = () => {}
+  
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+  const save = () => {
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
             name="name"
             type="text"
             placeholder="Enter Student Name"
@@ -17,13 +35,13 @@ export default function Form(props) {
               This must be a controlled component
             */
           />
-        </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={setInterviewer} />
+        </form >
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
