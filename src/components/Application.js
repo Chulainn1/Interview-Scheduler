6d45960e48d4c54da61b3ec7b23a7a7ec085d926
Chulainn1 <leam.murphy1@gmail.com>
2021-07-08 14:35:24 -0600

    hardcoded appointments data and created a map for each appointment that returns the Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,11 +3,66 @@ import React, {useState, useEffect} from "react";
 import "components/Application.scss";
 
 import DayList from "components/DayList";
+import Appointment from "components/Appointment"
 
 const axios = require('axios');
 
 
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Jane",
+      interviewer: {
+        id: 2,
+        name:"Tori Malcolm",
+        avatar:"https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "John",
+      interviewer: {
+        id: 3,
+        name:"Mildred Nazir",
+        avatar:"https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: "last",
+    time: "4pm",
+    interview: {
+      student: "Jasmine",
+      interviewer: {
+        id: 4,
+        name:"Cohana Roy",
+        avatar:"https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
 
 export default function Application(props) {
 
@@ -17,13 +72,17 @@ export default function Application(props) {
   useEffect(() => {
     axios.get('/api/days')
       .then(({data: newDays}) => {
-        console.log(newDays);
         setDays(newDays)
       })  
       .catch(function (error) {
         console.log(error);
       })
   }, [])
+
+  const appointment = appointments.map(appointment => {
+    return <Appointment key={appointment.id} {...appointment}/>
+  })
+  console.log(appointment)
 
   return (
     <main className="layout">
@@ -48,7 +107,8 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        
+        {appointment}
       </section>
     </main>
   );
